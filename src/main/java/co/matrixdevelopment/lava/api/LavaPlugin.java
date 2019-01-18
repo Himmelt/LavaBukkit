f545b5097c240a584588041fe57398db86b815bc
ca1halman <minertimer123@gmail.com>
2019-01-18 15:22:55 -0500

    Api stuff? Sure.

--- src/main/java/co/matrixdevelopment/lava/api/LavaPlugin.java
+++ src/main/java/co/matrixdevelopment/lava/api/LavaPlugin.java
@@ -1,5 +1,24 @@
 package co.matrixdevelopment.lava.api;
 
+import java.util.ArrayList;
+
 public class LavaPlugin {
-    protected final LavaSpigot SPIGOT = new LavaSpigot();
+    private ArrayList<LavaPlugin> dependencies = new ArrayList<>();
+
+    public void onEnable() {
+    }
+
+    public void onDisable() {
+    }
+
+    /**
+     * Be careful with this method, it runs on the main server thread! (for now)
+     */
+    public void onUpdate() {
+
+    }
+
+    public void dependsOn(LavaPlugin lp) {
+        dependencies.add(lp);
+    }
 }
